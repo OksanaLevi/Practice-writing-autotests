440c15a9536d190de31eb43e3f03239a5995c7d1
Oksana Levinskaya <qa.oksanaa@gmail.com>
2023-10-31 22:13:50 +0300

    added test with tad "hello"

--- src/test/java/com/demoqa/tests/SystemPropertiesTests.java
+++ src/test/java/com/demoqa/tests/SystemPropertiesTests.java
@@ -49,4 +49,23 @@ public class SystemPropertiesTests {
         // gradle property_test -Dbrowser=opera
         // opera
     }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+
+        System.out.println(message);
+        // gradle hello_test
+        // Hello, default student!
+
+        // gradle hello_test -Dname=Alex Egorov
+        // BUILD FAILED: Task 'Egorov' not found in root project 'demoqa-tests-19'.
+
+        // gradle hello_test -Dname="Alex Egorov"
+        // gradle hello_test "-Dname=Alex Egorov"
+        // Hello, Alex Egorov!
+
+    }
 }
