e6018ab7948419c0ce0d40782027cac6a5b1c3cc
Oksana Levinskaya <qa.oksanaa@gmail.com>
2023-10-31 21:57:34 +0300

    added SystemPropertiesTests

--- src/test/java/com/demoqa/tests/RegistrationRemoteTests.java
+++ src/test/java/com/demoqa/tests/RegistrationRemoteTests.java
@@ -51,7 +51,7 @@ class RegistrationRemoteTests extends RemoteTestBase  {
             $("#subjectsWrapper").$(byText("English")).click();
             $("#hobbiesWrapper").$(byText("Reading")).click();
 //      $("#uploadPicture").uploadFromClasspath("1.jpeg");
-            //В таком случае картинка будет подтягиваться по имени файла из папки resources. Конечно, картинку туда нужно положить и запушить на гитхаб :)
+//В таком случае картинка будет подтягиваться по имени файла из папки resources. Конечно, картинку туда нужно положить и запушить на гитхаб :)
             $("#currentAddress").setValue("Current address");
             $("#state").click();
             $("#stateCity-wrapper").$(byText("NCR")).click();
